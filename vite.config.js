2bb970646155130d7bb2a638197556a439c7fe01
Natthapong Kanna <lnwtor2541@gmail.com>
2024-10-20 16:01:22 +0700

    secoun commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // Bind to all network interfaces (0.0.0.0)
+    port: 80,    // Run on port 80 inside the container
+  },
 })
